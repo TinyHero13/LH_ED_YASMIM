e506839edc5a07c9f73321759765fc740e59c09f
TinyHero13 <yasmimcharles@protonmail.com>
2025-01-27 08:42:43 -0300

    feat: added task from tap-postgres to target csv

--- meltano_elt/orchestrate/airflow/dags/meltano_dag_generator.py
+++ meltano_elt/orchestrate/airflow/dags/meltano_dag_generator.py
@@ -16,14 +16,44 @@ DEFAULT_ARGS = {
     "catchup": False
 }
 
+tables = [
+    "categories",
+    "customer_customer_demo",
+    "customer_demographics",
+    "customers",
+    "employee_territories",
+    "employees",
+    "orders",
+    "products",
+    "region",
+    "shippers",
+    "territories",
+    "us_states"
+]
+
+
 with DAG(
     dag_id="indicium-northwind-elt",
     start_date=datetime(2025, 1, 1),
     schedule="@daily",
 ):
-    path = folder_verify('csv')
     
     csv_to_csv = BashOperator(
         task_id="csv_to_csv",
-        bash_command=f"cd {PROJECT_ROOT}; meltano config target-csv set output_path {path}; meltano el tap-csv target-csv"
+        bash_command=f"""cd {PROJECT_ROOT}; 
+                        meltano config target-csv set output_path {folder_verify('csv')}; 
+                        meltano el tap-csv target-csv"""
+    )     
+
+    postgres_to_csv = BashOperator(
+        task_id="postgres_to_csv",
+        bash_command=f"""
+            cd {PROJECT_ROOT};
+            for table in {' '.join(tables)};
+            do
+                mkdir -p ../data/postgres/$table/$(date +%F);
+                meltano config target-csv set output_path ../data/postgres/$table/$(date +%F);
+                meltano el tap-postgres target-csv --select "public-$table.*";
+            done
+        """
     )
